6c2143b43c6dadde1d913d57c09cfc6f4dee8456
Wei Xiao <wxiao8@stevens.edu>
2026-02-11 16:34:39 -0500

    HW03b add mock tests

--- HW 03a/test/test_hw03a.py
+++ HW 03a/test/test_hw03a.py
@@ -1,23 +1,66 @@
 from project.main import get_repo_commits
+from unittest.mock import patch, Mock
+def mock_response(json_data, status = 200):
+    m = Mock()
+    m.status_code = status
+    m.json.return_value = json_data
+    return m
+
+# test
+@patch("project.main.requests.get")
+def test_mock_success(mock_get):
+    # first to use repos list
+    repos_json = [
+        {"name": "repoA"},
+        {"name": "repoB"},
+        {"name": "repoC"},
+    ]
+
+    # latter use commits
+    commits_json_A = [{}, {}, {}]
+    commits_json_B = [{},]
+    commits_json_C = [{}, {}]
 
 
-def test_user_exists():
-    data = get_repo_commits("Wei-Xiao-git")
+    mock_get.side_effect = [
+        mock_response(repos_json),
+        mock_response(commits_json_A),
+        mock_response(commits_json_B),
+        mock_response(commits_json_C),
+    ]
 
-    assert isinstance(data, list)
-    assert len(data) > 0
+    result = get_repo_commits("test_user")
+    assert result == [
+        ("repoA", 3),
+        ("repoB", 1),
+        ("repoC", 2),
+    ]
 
+@patch("project.main.requests.get")
+def test_mock_invalid_user(mock_get):
 
-def test_repo_format():
-    data = get_repo_commits("Wei-Xiao-git")
+    mock_get.return_value = mock_response({}, 404)
 
-    repo_name, commit_count = data[0]
+    result = get_repo_commits("invalid_user")
+    assert result == []
 
-    assert isinstance(repo_name, str)
-    assert isinstance(commit_count, int)
-
-
-def test_invalid_user():
-    data = get_repo_commits("this_user_should_not_exist_123456")
-
-    assert data == []
+# def test_user_exists():
+#     data = get_repo_commits("Wei-Xiao-git")
+#
+#     assert isinstance(data, list)
+#     assert len(data) > 0
+#
+#
+# def test_repo_format():
+#     data = get_repo_commits("Wei-Xiao-git")
+#
+#     repo_name, commit_count = data[0]
+#
+#     assert isinstance(repo_name, str)
+#     assert isinstance(commit_count, int)
+#
+#
+# def test_invalid_user():
+#     data = get_repo_commits("this_user_should_not_exist_123456")
+#
+#     assert data == []
